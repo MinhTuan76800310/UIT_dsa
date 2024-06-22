0227765a4bf00055114f18622f357ba222a2c7cb
Pham Minh Tuan <tuan090611@gmail.com>
2024-06-22 11:47:38 +0700

    complete HighOfNode

--- BST/HighOfNode.cpp
+++ BST/HighOfNode.cpp
@@ -6,6 +6,14 @@
   * @date    20/06/2024
   ******************************************************************************
 */
+
+/*
+Problem:
+- Find the height of a node in a binary search tree.
+	+ If the node is not found, return -1.
+	+ If the node is the root, return 0.
+	+ If the tree is empty, print "Empty Tree."
+*/
 #include <iostream>
 using namespace std;
 
@@ -16,11 +24,72 @@ struct TNODE {
 };
 typedef TNODE* TREE;
 
-// Start
+TNODE* CreateNode(int x)
+{
+    TNODE* p = new TNODE;
+    p->key = x;
+    p->pLeft = p->pRight = NULL;
+    return p;
+}
 
+void PrintLNR(TREE T)
+{
+    if (T == NULL)
+    {
+        return;
+    }
+    PrintLNR(T->pLeft);
+    cout << T->key << " ";
+    PrintLNR(T->pRight);
+}
 
+void AddNode(TREE &T, int x)
+{
+    if (T == NULL) {
+        T = CreateNode(x);
+    } else if (x < T->key) {
+        AddNode(T->pLeft, x);
+    } else {
+        AddNode(T->pRight, x);
+    }
+}
 
-// End
+void CreateTree(TREE &T)
+{
+    int x;
+    while (true)
+    {
+        cin >> x;
+        if (x == -1)
+        {
+            break;
+        }
+        AddNode(T, x);
+    }
+}
+
+int HeightOfNode(TREE T, int x)
+{
+    int height = 0;
+    TNODE *iteratorNode = T;
+    while (iteratorNode != NULL)
+    {
+        if (x == iteratorNode->key)
+        {
+            return height;
+        }
+        else if (x > iteratorNode->key)
+        {
+            iteratorNode = iteratorNode->pRight;
+        }
+        else
+        {
+            iteratorNode = iteratorNode->pLeft;
+        }
+        height++;
+    }
+    return -1;
+}
 
 
 int main() {

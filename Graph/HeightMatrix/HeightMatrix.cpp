9aaf5972db4acbe82f564d154f82b7c6832d463d
Pham Minh Tuan <tuan090611@gmail.com>
2024-06-23 16:17:12 +0700

    Input by type

--- Graph/HeightMatrix/HeightMatrix.cpp
+++ Graph/HeightMatrix/HeightMatrix.cpp
@@ -8,79 +8,42 @@
 */
 #include <iostream>
 #include <vector>
-#include <unordered_map>
-#include <fstream>
+#include <map>
 #include <string>
-
 using namespace std;
-
 
 int main()
 {
-    int vertex, edge;
-
-    //          FILE
-    ifstream inputFile("testcase1.txt");
-    if (!inputFile.is_open())
-    {
-        cout << "Error opening file !";
-        return 1;
-    }
-
-    inputFile >> vertex;
-    inputFile >> edge;
-    inputFile.ignore();
+    int edge, vertex;
+    cin >> edge >> vertex;
     vector<vector<int>> AdjacentMatrix(vertex, vector<int>(vertex, 0));
-	unordered_map<string, int> vertexIdx(vertex);
-	vector<string> vertexName(vertex);
+    map<string, int> vertexIdx;
+    string vertexName;
 
-    // Read vector vertex
     for (int i = 0; i < vertex; i++)
     {
-        inputFile >> vertexName[i];
-        vertexIdx[vertexName[i]] = i;
+        cin >> vertexName;
+        vertexIdx[vertexName] = i;
     }
 
-
-    // read weight
-    // v v w
     string startVer, endVer;
-    int startIdx, endIdx;
-    int weight;
+    int startIdx, endIdx, weight;
     for (int i = 0; i < edge; i++)
     {
-        inputFile >> startVer;
-        inputFile >> endVer;
-        inputFile >> weight;
+        cin >> startVer >> endVer >> weight;
         startIdx = vertexIdx[startVer];
         endIdx = vertexIdx[endVer];
-
         AdjacentMatrix[startIdx][endIdx] = weight;
     }
-    inputFile.close();
 
-    /*
-    //                     TYPE
-	cin >> vertex >> edge;
-	cin.ignore();
-	for (int i = 0; i < vertex; i++)
+    for (int i = 0; i < vertex; i++)
     {
-        getline(cin, vertexName[i]);
-        vertexIdx[vertexName[i]] = i;
-    }
-
-
-	Input(v, v_index, e, G);
-	Output(v, v_index, e, G);
-    */
-    //              PRINT
-    for (int i =0; i < vertex; i++)
-    {
-        for (int j= 0; j < vertex; j++)
+        for (int j = 0; j < vertex; j++)
         {
             cout << AdjacentMatrix[i][j] << " ";
         }
         cout << endl;
     }
-	return 0;
+
+    return 0;
 }
